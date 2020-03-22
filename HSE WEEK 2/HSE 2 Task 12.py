2d9d7879065026d1822b23bdc7c2b50e0d1474c5
vlivon <61311919+vlivon@users.noreply.github.com>
2020-03-22 17:39:01 +0300

    Update HSE 2 Task 12.py

--- HSE WEEK 2/HSE 2 Task 12.py
+++ HSE WEEK 2/HSE 2 Task 12.py
@@ -1,10 +1,19 @@
-vertical1 = int(input())
-horizon1 = int(input())
-vertical2 = int(input())
-horizon2 = int(input())
-if vertical1 + 1 == vertical2 and horizon1 + 1 == horizon2:
-    print('YES')
-elif vertical1 - 1 == vertical2 and horizon1 + 1 == horizon2:
-    print('YES')
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+if 8 >= x1 >= 1 and 8 >= y1 >= 1:
+    if 8 >= x2 >= 1 and 8 >= y2 >= 1:
+        if (x1 + y1 + x2 + y2) % 2 == 0:
+            if y2 > y1:
+                print('YES')
+            else:
+                print('NO')
+        elif (x1 + y1 + x2 + y2) % 2 == 0:
+            print('YES')
+        else:
+            print('NO')
+    else:
+        print('NO')
 else:
     print('NO')
